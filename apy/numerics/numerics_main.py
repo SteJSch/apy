eda4f8a288db13b2e6767ef323700f323e05ab49
Stefan Schaefer <a3497@undumachine.basisit.de>
2024-03-06 11:43:29 +0100

    [v0]

--- apy/numerics/numerics_main.py
+++ apy/numerics/numerics_main.py
@@ -3,3 +3,14 @@ apy is a litle python wrapper build for easy use of the program WAVE by Michael 
 Import by writing: import wavepy as wpy
 """
 
+class numerics_class() :
+	def __init__(self) : 
+		b=2
+
+class fast_fourier_transform()
+	def __init__(self) : 
+		b=2
+		phase_fun = b
+		fac_fun = a
+		acc = a
+
