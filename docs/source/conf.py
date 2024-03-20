004a2db06034365cabf69e2e9fa013d4cf24b665
Stefan Schaefer <a3497@undumachine.basisit.de>
2024-03-20 14:39:24 +0100

    [A test commit]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = 'Sphinx Template'
 copyright = '2024, Johnny Cash'
 author = 'Beyonce'
-release = 'v0.0.3'
+release = 'v0.0.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
